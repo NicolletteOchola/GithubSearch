f5a89b61a5d2ffb51741ccd895161f828f2e1880
Nicole Ocholla <nicoleochola@gmail.com>
2019-12-09 01:54:50 +0300

    Created a pipe for generting dates.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalcDatePipe } from './calc-date.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcDatePipe
   ],
   imports: [
     BrowserModule,
--- src/app/repository.ts
+++ src/app/repository.ts
@@ -1,2 +1,10 @@
 export class Repository {
+  constructor (
+    public name:string,
+    public description: string,
+    public forks: number,
+    public html_url: string,
+    public created_at : Date
+
+  )
 }
