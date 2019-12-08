72c099bae03360aa31f5dcd660414e16e9c31289
Nicole Ocholla <nicoleochola@gmail.com>
2019-12-09 01:39:39 +0300

    Created a constructor for the users details.

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,13 @@
 export class User {
+  constructor (
+    public name: string,
+    public public_repository: number, 
+    public avatar_url: string, 
+    public login: string, 
+    public company: string, 
+    public created_at: Date, 
+    public following: number, 
+    public followers: number,
+    public html_url:string
+    )
 }
