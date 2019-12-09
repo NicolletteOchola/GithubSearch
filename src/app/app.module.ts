5e664a059440995a23d84809b75066c1ac18b33a
Nicole Ocholla <nicoleochola@gmail.com>
2019-12-09 03:47:59 +0300

    Added a component for the main page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { CalcDatePipe } from './calc-date.pipe';
 import { RepositoryComponent } from './repository/repository.component';
 import { SearchFormComponent } from './search-form/search-form.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     CalcDatePipe,
     RepositoryComponent,
     SearchFormComponent,
-    NavbarComponent
+    NavbarComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
