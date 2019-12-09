a21bf14b9dfa111c9293cc095d3e77a0d2b7331a
Nicole Ocholla <nicoleochola@gmail.com>
2019-12-09 04:00:17 +0300

    Gave the main page  info from the services

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user-service/user.service';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user';
+import { RepositoryService } from '../repository-service/repository.service';
 
 @Component({
   selector: 'app-landing',
@@ -7,8 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  profile: any;
+  repos: any;
+  userName: string;
 
+  constructor(private userService: UserService, private repositoryService: RepositoryService) { 
+    this.userService.getUsers().subscribe(profile => {
+      console.log(profile);
+      this.profile = profile;
+    });
+
+    this.userService.getUsers().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
+  }
+
+  findUser() {
+    this.userService.updateUser(this.userName);
+    this.userService.getUserInfo()
+    this.profile = this.userService.profile;
+
+    this.repositoryService.updateUser(this.userName);
+    this.repositoryService.getRepoInformation()
+    this.repos = this.repositoryService.repos;
+  }
   ngOnInit() {
   }
 
